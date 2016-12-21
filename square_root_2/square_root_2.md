a62b06a895d215ef571f58adf944ff1f31459dff
LafeLabs <LafeLabs@gmail.com>
2016-12-20 23:04:05 -0500

    root 2 mostly done but need a ton more examples and explanation of the math

--- square_root_2/square_root_2.md
+++ square_root_2/square_root_2.md
@@ -1,3 +1,78 @@
 # Square Root of Two, and Divided Angles
 
 It's time to add more flexibility to the system of angles.  To that end, we're going to add two more angle manipulation commands, one which divides the current thetaStep by two and one which multiplies by 2.  Note that since we're starting with thetaStep being always 90 degrees, the first application of this is to divide by 2 to get a 45 degree angle.  That is very useful!  If we also add a divide by 3 and multiply by 3, we can get most of the angles we care about, such as 30 degrees and 60 degrees as well.  In addition to these we will add one more drawing command, which will draw an arc across the span of the cursor.  
+
+Also, note that since the main point of this section is to add 45 degree angles we are going to really want to be able to multiply and divide by factors of the square root of two.  This will make it easy to make isosceles right triangles of all sizes and angles.   
+
+So in total we are adding 6 commands: halve angle(bisect), double angle, trisect angle, triple angle, draw arc, and set scaleFactor to sqrt(2).  Each of these will need its own glyph.  The angle manipulation commands will all go in the 0350's row of the octal address space.  Draw arc lives on the 0340's line next to the other draw commands, and setting multiples for distance all go in the 0310's line below the symmetry settings and operational commands like reset.  
+
+Angle manipulation commands indicate the "from" and "to" angles with solid and dotted lines, respectively, and are drawn to make the proportions as obvious as possible as follows:
+
+![](root2newglyphs.png)
+
+    if(localCommand == 0310){
+       scaleFactor = sqrt(2); 
+    }
+    
+    if(localCommand == 0343){
+        arc(x,y,2*side,2*side,theta - thetaStep,theta + thetaStep);//arc
+    }
+    
+    if(localCommand == 0350){
+      thetaStep /= 2;  //angle/2
+    }
+    if(localCommand == 0351){
+      thetaStep *= 2;  //2angle
+    }
+    if(localCommand == 0352){
+      thetaStep /= 3; //angle/3
+    }
+    if(localCommand == 0353){
+      thetaStep *= 3; //3angle
+    }
+
+
+the above adds the six new commands to doTheThing(), and now we add them to keyPressed():
+
+
+	 if(key == 'j'){
+     	currentGlyph = append(currentGlyph,0350);                   
+     }
+     if(key == 'k'){
+     	currentGlyph = append(currentGlyph,0351);                   
+     }
+     if(key == 'l'){
+     	currentGlyph = append(currentGlyph,0352);                   
+     }
+     if(key == ';'){
+     	currentGlyph = append(currentGlyph,0353);                   
+     }
+     if(key == '2'){
+     	currentGlyph = append(currentGlyph,0313);                   
+     }
+     if(key == '1'){
+     	currentGlyph = append(currentGlyph,0310);                   
+     }
+     if(key == 'b'){
+     	currentGlyph = append(currentGlyph,0343);                  
+     }
+
+
+Also we want to add the following line to the 0300 command:
+
+      thetaStep = PI/2;
+to make sure each iteration of the glyph starts out clean and the angle does't either infinitely shrink or grow.  
+
+Here's a screenshot:
+
+![](root2screenshot2.png)
+
+It is now very straight forward to construct letters in English as well as most other languages.  
+
+
+
+
+code goes here:
+
+[square_root_2.pde](square_root_2/square_root_2.pde)
+
---
+++ square_root_2/square_root_2/application.windows32/source/square_root_2.java
@@ -0,0 +1,243 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class square_root_2 extends PApplet {
+
+//EVERYTHING IS PHYSICAL
+//EVERYTHING IS GLOBAL
+//EVERYTHING IS ALWAYS RECURSIVE
+//NO LAWS NO PROPERTY NO MINING NO NUMBERS NO MINERALS
+//THE SOLE PURPOSE OF THE EXISTING SOFTWARE INDUSTRY IS TO CREATE AND MAINTAIN STRUCTURAL VIOLENCE, \
+//ALL COMPUTER "SCIENCE" IS EVIL
+//ALL "TECH" COMPANIES ARE BASED ON FRAUD AND LIES
+//SMASH THE TECHNOCRATIC PRIESTHOOD
+
+float x,y,x0,y0;
+float side;
+float scaleFactor, unit;
+float theta,theta0,thetaStep;
+int[] currentGlyph = {};
+
+public void setup(){
+  ellipseMode(CENTER);
+  noFill();
+  strokeWeight(1);
+  unit = 50;
+  scaleFactor = 2;
+  side = 50;
+  thetaStep = PI/2;
+  theta0 = -PI/2; 
+  theta = theta0;
+  
+  x0 = 250;
+  y0 = 250;
+  x = x0;
+  y = y0;
+}
+
+public void draw(){
+ background(255);
+ image(loadImage("root2fullkey.png"),0,0,500,170);
+ doTheThing(0300);
+ drawGlyph(currentGlyph);
+ drawCursor();
+}
+
+public void drawCursor(){
+  stroke(color(255,0,0));
+  line(x,y,x + scaleFactor*side*cos(theta),y+scaleFactor*side*sin(theta));
+  strokeWeight(3);
+  stroke(0);
+  line(x,y,x + side*cos(theta),y + side*sin(theta));
+  stroke(color(0,0,255));
+  strokeWeight(4);
+  line(x,y,x + (side/scaleFactor)*cos(theta),y+(side/scaleFactor)*sin(theta));
+  strokeWeight(1);
+  stroke(0);
+  line(x,y,x + side*cos(theta + thetaStep),y+side*sin(theta + thetaStep));
+  line(x,y,x + side*cos(theta - thetaStep),y+side*sin(theta - thetaStep));
+}
+
+public void keyPressed(){
+   if(key == 'a'){
+     currentGlyph = append(currentGlyph,0330);
+  }
+  if(key == 's'){
+     currentGlyph = append(currentGlyph,0331);
+  }
+  if(key == 'd'){
+     currentGlyph = append(currentGlyph,0332);
+  }
+  if(key == 'f'){
+     currentGlyph = append(currentGlyph,0333);
+  }
+  if(key == 'g'){
+     currentGlyph = append(currentGlyph,0334);
+  }
+  if(key == 'h'){
+     currentGlyph = append(currentGlyph,0335);
+  }
+  if(key == 'z'){
+     currentGlyph = append(currentGlyph,0340);
+  }
+  if(key == 'x'){
+     currentGlyph = append(currentGlyph,0341);
+  }
+  if(key == 'c'){
+     currentGlyph = append(currentGlyph,0342);
+  }
+  if(key == '0'){
+     currentGlyph = append(currentGlyph,0300);   
+  }
+  if(key == 'v'){
+     currentGlyph = append(currentGlyph,0360);         
+  }
+   if(key == '-'){
+     currentGlyph = append(currentGlyph,0336);               
+  }
+  if(key == '='){
+     currentGlyph = append(currentGlyph,0337);               
+  }
+  if(key == 'j'){
+     currentGlyph = append(currentGlyph,0350);                   
+  }
+  if(key == 'k'){
+     currentGlyph = append(currentGlyph,0351);                   
+  }
+  if(key == 'l'){
+     currentGlyph = append(currentGlyph,0352);                   
+  }
+  if(key == ';'){
+     currentGlyph = append(currentGlyph,0353);                   
+  }
+  if(key == 'r'){
+     currentGlyph = append(currentGlyph,0313);                   
+  }
+  if(key == 'q'){
+     currentGlyph = append(currentGlyph,0310);                   
+  }
+  if(key == 'b'){
+     currentGlyph = append(currentGlyph,0343);                   
+  }
+  if(key == 8){ //delete key
+    if(currentGlyph.length != 0){
+      currentGlyph = shorten(currentGlyph);
+    }
+  }
+}
+
+public void drawGlyph(int[] localGlyph){
+  for(int index = 0;index < localGlyph.length;index++){
+    doTheThing(localGlyph[index]);  
+  }  
+}
+
+public void doTheThing(int localCommand){
+    if(localCommand == 0300){
+      x = x0;
+      y = y0;
+      theta = theta0;
+      side = unit;
+      thetaStep = PI/2;
+    }
+    if(localCommand == 0304){
+      thetaStep = PI/2;
+    }
+    if(localCommand == 0310){
+       scaleFactor = sqrt(2); 
+    }
+    if(localCommand == 0313){
+      scaleFactor = 2;  //2x
+    }
+    if(localCommand == 0330){
+      x += side*cos(theta);   
+      y += side*sin(theta); 
+    }
+    if(localCommand == 0331){
+      x -= side*cos(theta);   
+      y -= side*sin(theta); 
+    }
+    if(localCommand == 0332){
+      x += side*cos(theta - thetaStep);
+      y += side*sin(theta - thetaStep);
+    }
+    if(localCommand == 0333){
+      x += side*cos(theta + thetaStep);
+      y += side*sin(theta + thetaStep);
+    }
+    if(localCommand == 0334){
+      theta -= thetaStep; // CCW
+    }
+    if(localCommand == 0335){
+      theta += thetaStep; // CW
+    }
+    if(localCommand == 0336){
+      side /= scaleFactor; // -
+    }
+    if(localCommand == 0337){
+      side *= scaleFactor; // +
+    }
+    if(localCommand == 0340){
+      strokeWeight(5);
+      point(x,y);
+      strokeWeight(1);//point
+    }
+    if(localCommand == 0341){
+      ellipse(x,y,2*side,2*side);//circle
+    }
+    if(localCommand == 0342){
+      line(x,y,x + side*cos(theta),y + side*sin(theta));//line
+    }
+    if(localCommand == 0343){
+        arc(x,y,2*side,2*side,theta - thetaStep,theta + thetaStep);//arc
+    }
+    if(localCommand == 0350){
+      thetaStep /= 2;  //angle/2
+    }
+    if(localCommand == 0351){
+      thetaStep *= 2;  //2angle
+    }
+    if(localCommand == 0352){
+      thetaStep /= 3; //angle/3
+    }
+    if(localCommand == 0353){
+      thetaStep *= 3; //3angle
+    }
+    if(localCommand == 0360){
+       int[] localGlyph = {0304,0342,0330,0334,0342,0330,0334,0342,0330,0334,0342,0330,0334}; 
+       drawGlyph(localGlyph);
+    }
+    if(x > width){
+      x=0;
+    }
+    if(x<0){
+       x = width; 
+    }
+    if(y>height){
+      y=0;
+    }
+    if(y<0){
+      y = height;
+    }
+}
+  public void settings() {  size(500,500); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "square_root_2" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
