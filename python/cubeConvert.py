44dd0f3cc779fe3850c3600436cc4609a09696c0
LafeLabs <LafeLabs@gmail.com>
2017-06-02 22:21:33 -0400

    from js to byte code

--- python/cubeConvert.py
+++ python/cubeConvert.py
@@ -9,7 +9,7 @@ def string2byteCode(inputstring):
             outbytes += "0o" + thisbyte.lstrip("0") + ","
         else:
             outbytes += thisbyte + ","
-    print outbytes
+    return outbytes
 
 def byteCode2string(inputcode):
     inputarray = inputcode.split(",")
@@ -35,17 +35,45 @@ def gCube2jsCube(cubeURL):#convert pastebin hypercube in geometron format into .
     return jsCodeOut
 
 
-def jsCube2gCube(jslibURL):#convert pastebin hypercube in geometron format into .js library in github folder
+def extractDoTheThing(jslibURL):#convert js library to  hypercube
     response = urllib2.urlopen(jslibURL)
     jsString = response.read()
-    linesOfCode = hypercubestring.split("\n")
-    gCubeOut = ""
+    linesOfCode = jsString.split("\n")
+    outputString = ""
+    inDoTheThing = False
     for index in range(len(linesOfCode)):
-        if len(inputarray[index]) > 0:
-    return gCubeOut
+        if "function localRoot(localCommand)" in linesOfCode[index]:
+            inDoTheThing = False
+        if inDoTheThing:
+            outputString += linesOfCode[index].rstrip("\r")
+            outputString += "\n"    
+        if linesOfCode[index] == "function doTheThing(localCommand){":
+            inDoTheThing = True
 
+    return outputString
+
+def doTheThing2cube(doTheThingString):
+    thingArray = doTheThingString.split("if(localCommand == ")
+    outputCube = ""
+    for index in range(len(thingArray)):
+        address = thingArray[index].partition(")")[0]  
+        address = address.lstrip()
+        address = address.lstrip("\r")
+        address = address.lstrip("\n")
+        address = address.lstrip()
+        code = thingArray[index].partition(")")[2] 
+        code =  code.lstrip("{")
+        code =  code.lstrip()
+        code = code.rpartition("}")[0]
+        code = code.rstrip()
+        if len(code) > 0:
+            outputCube += address + ":" + string2byteCode(code) + "\n"
+    outputCube =  "0500:" + outputCube.partition("0500:")[2]
+    print outputCube
 
 #print gCube2jsCube("https://pastebin.com/raw/fCABR3RA")
-print gCube2jsCube("https://pastebin.com/raw/qD7UUYc5")
+#print gCube2jsCube("https://pastebin.com/raw/qD7UUYc5")
 
+foo =  extractDoTheThing("https://lafelabs.github.io/cube3.html")
+doTheThing2cube(foo)
 
